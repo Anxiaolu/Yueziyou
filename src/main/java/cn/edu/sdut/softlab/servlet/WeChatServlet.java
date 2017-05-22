f382ef5f053c9f664f9af6d956b061c0166cfde3
Anxiaolu <1466022881@qq.com>
2017-05-22 21:43:49 +0800

    添加servlet的链接方式

--- src/main/java/cn/edu/sdut/softlab/servlet/WeChatServlet.java
+++ src/main/java/cn/edu/sdut/softlab/servlet/WeChatServlet.java
@@ -4,39 +4,44 @@ package cn.edu.sdut.softlab.servlet;
  * 获取值
  */
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import cn.edu.sdut.softlab.util.ChecktUtil;
+
 public class WeChatServlet extends HttpServlet {
+    
+    private static final long serialVersionUID = 1L;
 
     public WeChatServlet() {
         super();
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        MyWechat myWechat = new MyWechat(request);
-        String result = myWechat.execute();
-        response.getOutputStream().write(result.getBytes());
-//        String signature = req.getParameter("signature");
-//        String timestamp = req.getParameter("timestamp");
-//        String nonce = req.getParameter("nonce");
-//        String echostr = req.getParameter("echostr");
-//
-//        // 调用逻辑验证
-//        PrintWriter out = resp.getWriter();
-//        if (CheckUtil.checkSignature(signature, timestamp, nonce)) {
-//            System.out.println("check ok!");
-//            out.println(echostr);
-//        }
-//        out.close();
+//        MyWechat myWechat = new MyWechat(request);
+//        String result = myWechat.execute();
+//        response.getOutputStream().write(result.getBytes());
+        String signature = request.getParameter("signature");
+        String timestamp = request.getParameter("timestamp");
+        String nonce = request.getParameter("nonce");
+        String echostr = request.getParameter("echostr");
+
+        // 调用逻辑验证
+        PrintWriter out = response.getWriter();
+        if (ChecktUtil.checkSignature(signature, timestamp, nonce)) {
+            System.out.println("check ok!");
+            out.println(echostr);
+        }
+        out.close();
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        MyWechat myWechat = new MyWechat(request);
-        String result = myWechat.execute();
-        response.getOutputStream().write(result.getBytes());
+//        MyWechat myWechat = new MyWechat(request);
+//        String result = myWechat.execute();
+//        response.getOutputStream().write(result.getBytes());
     }
 }
